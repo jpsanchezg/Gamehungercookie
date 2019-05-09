7087d3515b13d0a85434f996e747e681e86eb092
juan pablo sanchez gaitan <50275221+lordcidiak@users.noreply.github.com>
2019-05-08 20:56:26 -0500

    Update main.cpp
    faltan cosas en las funciones de mapa y jugador

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include "stdlib.h"
 #include <iomanip>
 
-using namespace std;
+using namesapec std;
 
 #define maxc 30
 #define maxf 30
-void tablerocreado (int numeros[maxf][maxf], int nfilas, int ncol);
-void tableroaleatorio (int numeros[maxf][maxf], int nfilas, int ncol);
+void tablerocreado (char player [maxf][maxc],int numeros[maxf][maxf], int nfilas, int ncol);
+void tableroaleatorio (char player [maxf][maxc],int numeros[maxf][maxf], int nfilas, int ncol);
 void jugador ( char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, int ncol);
 void vistamundo ( char player [maxf][maxc], int numeros [maxf][maxc], int nfilas, int ncol);
 void sensacion (  char player [maxf][maxc], int numeros [maxf][maxc], int nfilas, int ncol);
@@ -49,10 +49,19 @@ int main ()
                 {
                     for(int j = 0; j<ncol; j++)
                     {
-                        numeros[i][j]=0;
+                        numeros[i][j]='0';
+                        
                     }
                 }
-                tablerocreado (numeros,nfilas,ncol);
+                   for(int i=0; i<nfilas; i++)
+    {
+        for (int j=0; j<ncol; j++)
+        {
+            cout<<setw(8)<<numeros [i][j];
+        }
+        cout<<endl;
+    }
+                tablerocreado (player, numeros,nfilas,ncol);
                 jugador (player,numeros,nfilas,ncol);
             }
             if (respu == 2)
@@ -82,113 +91,50 @@ break;
     }
     while (opcion != 4);
 }
-void tablerocreado (int numeros[maxf][maxf], int nfilas, int ncol)
+void tablerocreado (char player [maxf][maxc],int numeros[maxf][maxf], int nfilas, int ncol)
 {
-    int abis=0,alien=0,multi=0;
+    int abis=0,alienf=0,alienc=0,multi=0,ret=0;
+    int jugf=0, jugc=0;
+    int galletf=0,galletc=0;
+    bool poss=false;
     multi = nfilas* ncol;
-    cout<<"cuantos abismos desea poner?"<<" reduerde que tiene "<<multi<<" posiciones en el mapa"<<endl;
+    ret = multi/2;
+    cout<<"tienes  "<<multi<<" posiciones en el mapa para poner los abismos, el monstruo y la galleta"<<endl;
+    cout<<"En que filas vas a poner la galleta?"<<endl;
+    cin>>galletf;
+    cout<<"en que columna vas a poner la galleta?"<<endl;
+    cin>>galletc;
+    while(poss== false){
+       cout<<"en que fila quiere poner el alien "<<endl; 
+    cin>>alienf;
+        cout<<"en que columna va a ingresar el alien"<<endl;
+    cin>>alienc;
+    if (alienf == galletf && alienc == galletc){
+        cout<<"Valor ingresado incorrecto, es el mismo que el de la galleta"<<endl;
+        poss= false;}
+        
+    if (alienf != galletf && alienc != galletc){
+        poss= true;}
+    }
+    
+    cout<<"cuantos abismos va a poner recuerde que debe ser menos a "<<ret<<endl;
     cin>>abis;
-       cout<<"En que posicion va a poner el alien? recuerde que la posicion ustde no la sabe lo unico que va a poner es el numero 2"<<endl;
-    cin>>alien;
-    for(int i=0+rand()%nfilas; i<nfilas; i++)
-    {
-        for (int j=0+rand()%ncol; j<ncol; j++)
-        {
-
-                numeros [0][0]=0;
-
-
-                numeros [i][j]=alien;
-
-
-            if  (numeros [i][j] == 0)
-            {
-                if( numeros [i][j+1] == 0)
-                {
-                    if(numeros[i][j-1] == 0)
-                    {
-                        if (numeros [i+1][j] == 0)
-                        {
-                            if(numeros [i-1][j] == 0)
-                            {
-                                numeros[i][j]=3;
-
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    for(int i=0; i<nfilas; i++)
-    {
-        for (int j=0; j<ncol; j++)
-        {
-            cout<<setw(8)<<numeros [i][j];
-        }
-        cout<<endl;
-    }
+        |
+  
+   
 }
-void tableroaleatorio (int numeros[maxf][maxf], int nfilas, int ncol)
+void tableroaleatorio (char player [maxf][maxc],int numeros[maxf][maxf], int nfilas, int ncol)
 {
-     for(int i=0; i<nfilas; i++)
-    {
-        for (int j=1; j<ncol; j++)
-        {
-
-                numeros [0][0]=0;
-
-
-                numeros [i][0+rand()%(ncol)]=1;
-
-
-            if  (numeros [i][j] == 0)
-            {
-                if( numeros [i][j+1] == 0)
-                {
-                    if(numeros[i][j-1] == 0)
-                    {
-                        if (numeros [i+1][j] == 0)
-                        {
-                            if(numeros [i-1][j] == 0)
-                            {
-                                numeros[i][j]=3;
-
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+   todavia no hay nbada jajajaj
     
 }
 void jugador ( char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, int ncol)
 {
-for(int i =0;i<nfilas;i++)
-{
-    for(int j=0;j<ncol;j++)
-    {
-        cin>>player[i][j]
-        
-        if (player [i][j]=='d')
-        {
-            player [i][j+1];
-        }
-        if(player[i][j]=='i'){
-            playr[i][j-1];
-        }
-        if (player[i][j]=='a'){
-            player[i-1][j];
-        }
-        if(player[i][j]=='b'){
-            player[i+1][j];
-        
-        }
-        
-        
-    }
-}
+char position;
+    cout<<"Ingrese un movimiento o accion"<<endl;
+    cout<<"ARRIBA = a"<<endl;
+    
+    
 }
 void (  char player [maxf][maxc], int numeros [maxf][maxc], int nfilas, int ncol)
 {
