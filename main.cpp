8ca4aef14dda7148d2182d550f1b026e94b12c49
juan pablo sanchez gaitan <50275221+lordcidiak@users.noreply.github.com>
2019-05-19 21:15:31 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,22 +5,21 @@
 #include <iomanip>
 //juan pablo sanchez gaitan
 using namespace std;
-struct personaje{
-char movs;
+struct personaje
+{
+    char movs;
     char galleta;
 };
 
 #define maxc 30
 #define maxf 30
-void tablerocreado (struct personaje,int numeros[maxf][maxf], int nfilas, int ncol);
+void tablerocreado (char player [maxf][maxc],int numeros[maxf][maxf], int nfilas, int ncol);
 void tableroaleatorio (char player [maxf][maxc],int numeros[maxf][maxf], int nfilas, int ncol);
 void jugador ( char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, int ncol,int jugf,int jugc);
 void vistamundo ( char player [maxf][maxc], int numeros [maxf][maxc], int nfilas, int ncol);
-void sensacion (  char player [maxf][maxc], int numeros [maxf][maxc], int nfilas, int ncol);
+void sensacion (  char player [maxf][maxc], int numeros [maxf][maxc], int nfilas, int ncol,int pojf,int pojc);
 void disparar (  char player [maxf][maxc], int numeros [maxf][maxc], int nfilas, int ncol);
-void imprimirjugador (char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, int ncol);
-void alien ( char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, int ncol);
-void gameover  ( char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, int ncol);
+void gameover  ( char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, int ncol,int pojf,int pojc);
 
 int main ()
 {
@@ -28,6 +27,7 @@ int main ()
     int jugf=0, jugc=0;
     srand(time(NULL));
     char player [maxf][maxc];
+    personaje lucas;
 
     do
     {
@@ -134,13 +134,13 @@ void tablerocreado (char player [maxf][maxc],int numeros[maxf][maxf], int nfilas
         while(poss== false)
         {
 
-            if (alienf == abisf && alienc == abisc)
+            if (numeros[galletf][galletc] == numeros[abisf][abisc] )
             {
                 cout<<"Valor ingresado incorrecto, es el mismo que el del alienigena"<<endl;
                 poss= false;
             }
 
-            if (alienf != abisf && alienc != abisc)
+            if (numeros[galletf][galletc] !=  numeros[abisf][abisc])
             {
 
                 poss= true;
@@ -158,47 +158,56 @@ void tablerocreado (char player [maxf][maxc],int numeros[maxf][maxf], int nfilas
 }
 void tableroaleatorio (char player [maxf][maxc],int numeros[maxf][maxf], int nfilas, int ncol)
 {
-    int abis=0 + rand ()% (nfilas),alienf=0 + rand ()% (nfilas),alienc=0 + rand ()% (ncol);
-    int multi=0,ret=0,filas=0,col=0;
+
+    int abis=0,alienf=0,alienc=0,multi=0,ret=0,filas=0,col=0;
     int jugf=0, jugc=0;
-    int perf=0,perc=0;
     player[jugf][jugc]='L';
-    int galletf=0 + rand ()% (nfilas),galletc=0 + rand ()% (ncol);
+    numeros[jugf][jugc]= 5;
+    int galletf=0,galletc=0;
     bool poss=false;
     multi = nfilas* ncol;
     ret = multi/2;
+    // poricion aleatoria del alien
+    alienf =  0 + rand ()% (nfilas);
+    alienc =  0 + rand ()% (ncol);
+    numeros[alienf][alienc]=1;
+    //posicion aleatoria de la galleta
+    galletf=0 + rand ()% (nfilas);
+    galletc =0 + rand ()% (ncol);
+        numeros[galletf][galletc]=3;
 
-    numeros[galletf][galletc]=3;
-
-    while(poss== false)
+    abis =  0 + rand ()% (ret);
+    int k=0,abisf=0,abisc=0;
+    do
     {
-        if (alienf == galletf && alienc == galletc)
-        {
-            poss= false;
-        }
+        k++;
+        abisf =  0 + rand ()% (nfilas);
+        abisc =  0 + rand ()% (ncol);
 
-        if (alienf != galletf && alienc != galletc)
+
+        while(poss== false)
         {
-            poss= true;
-            numeros[alienf][alienc]=1;
-        }
-    }
-    perf= jugf +1;
-    perc = jugc +1;
-    for(int i=0; i<abis; i++)
-    {
-        filas = 0 + rand ()% (nfilas);
-        col = 0 + rand ()% (ncol);
-        if (filas != perf && col != perc)
-        {
-            if (filas != alienf && col != alienc)
+
+            if (numeros[galletf][galletc] == numeros[abisf][abisc] )
             {
-                numeros[filas][col]=2;
+                poss= false;
+            }
+
+            if (numeros[galletf][galletc] !=  numeros[abisf][abisc])
+            {
+
+                poss= true;
+                numeros[abisf][abisc]=2;
+
             }
         }
     }
-    system("cls");
+    while(k<abis);
+
+
     vistamundo (player,numeros,nfilas,ncol);
+    jugador (player,numeros,nfilas,ncol,jugf,jugc);
+
 
 
 }
@@ -217,90 +226,146 @@ void vistamundo (char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, i
 void jugador ( char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, int ncol, int jugf,int jugc)
 {
     char opcion;
+    int contad =0,contai =0;
     int pojf=0,pojc=0;
     bool muerto = false;
-    cout<<"Ingrese un movimiento o accion"<<endl;
-    cout<<"GIRAR A LA DERECHA = D"<<endl;
-    cout<<"GIRAR IZQUIERDA = I"<<endl;
-    cout<<"AVANZAR = A"<<endl;
-    cout<<"DISPARAR = F"<<endl;
-    cout<<"RECOGER GALLETA = R"<<endl;
-    cin>>opcion;
-
-    switch (opcion)
+    do
     {
-    case 'a':
-    case 'A':
-        sensacion (   player, numeros,nfilas, ncol);
-        if(opcion == 'd'))
+        cout<<"Ingrese un movimiento o accion"<<endl;
+        cout<<"GIRAR A LA DERECHA = D"<<endl;
+        cout<<"GIRAR IZQUIERDA = I"<<endl;
+        cout<<"AVANZAR = A"<<endl;
+        cout<<"DISPARAR = F"<<endl;
+        cout<<"RECOGER GALLETA = R"<<endl;
+        cout<<"SALIR = S"<<endl;
+        cin>>opcion;
+        if (opcion == 'd')
         {
-            switch(numeros[pojf][pojc])
+            contad++;
+        }
+        if (opcion == 'i')
+        {
+            contai++;
+        }
+        if (opcion == 'a')
+        {
+            player [pojf][pojc]='-';
+            numeros[pojf][pojc] = 0;
+            if (contad == 1)
             {
-            case 1:
+                pojc = pojc +1;
+                player [pojf][pojc]='D';
+                numeros [pojf][pojc]=5;
+                vistamundo (player,numeros,nfilas,ncol);
+                sensacion (  player,  numeros,  nfilas, ncol,pojf,pojc);
+                                gameover (player,numeros,nfilas,ncol);
 
-                alien(player,numeros,nfilas,ncol,jugf,jugc);
 
-                break;
-            case 2:
-                gameover (player,numeros,nfilas,ncol,jugf,jugc);
-                break;
-            case 3:
-                galleta (player,numeros,nfilas,ncol,jugf,jugc);
-                break;
-            default:
-                player[jugf][jugc]= 'a';
-                numeros[jugf][jugc]= 5;
-                break;
+                contad=0;
+            }
+            if (contad == 2)
+            {
+                pojf = pojf +1;
+                player [pojf][pojc]='B';
+                numeros [pojf][pojc]=5;
+                vistamundo (player,numeros,nfilas,ncol);
+                sensacion (  player,  numeros,  nfilas, ncol,pojf,pojc);
+
+                gameover (player,numeros,nfilas,ncol);
+                contad=0;
+            }
+            if (contad == 3)
+            {
+                pojc = pojc -1;
+                player [pojf][pojc]='a';
+                numeros [pojf][pojc]=5;
+                vistamundo (player,numeros,nfilas,ncol);
+                sensacion (  player,  numeros,  nfilas, ncol,pojf,pojc);
+                gameover (player,numeros,nfilas,ncol);
+
+                contad=0;
+            }
+            if (contad == 4)
+            {
+                pojf = pojf -1;
+                player [pojf][pojc]='a';
+                numeros [pojf][pojc]=5;
+                vistamundo (player,numeros,nfilas,ncol);
+                sensacion (  player,  numeros,  nfilas, ncol,pojf,pojc);
+                gameover (player,numeros,nfilas,ncol);
+
+                contad=0;
+            }
+            if (contai == 1)
+            {
+                pojc = pojc -1;
+                player [pojf][pojc]='I';
+                numeros [pojf][pojc]=5;
+                vistamundo (player,numeros,nfilas,ncol);
+                sensacion (  player,  numeros,  nfilas, ncol,pojf,pojc);
+                gameover (player,numeros,nfilas,ncol);
+                contai=0;
+            }
+            if (contai == 2)
+            {
+                pojf = pojf +1;
+                player [pojf][pojc]='B';
+                numeros [pojf][pojc]=5;
+                vistamundo (player,numeros,nfilas,ncol);
+                sensacion (  player,  numeros,  nfilas, ncol,pojf,pojc);
+                gameover (player,numeros,nfilas,ncol);
+
+                contai=0;
+            }
+            if (contai == 3)
+            {
+                pojc = pojc +1;
+                player [pojf][pojc]='a';
+                numeros [pojf][pojc]=5;
+                vistamundo (player,numeros,nfilas,ncol);
+                sensacion (  player,  numeros,  nfilas, ncol,pojf,pojc);
+                                gameover (player,numeros,nfilas,ncol);
+
+                contai=0;
+            }
+            if (contai == 4)
+            {
+                pojf = pojf -1;
+                player [pojf][pojc]='a';
+                numeros [pojf][pojc]=5;
+                vistamundo (player,numeros,nfilas,ncol);
+                sensacion (  player,  numeros,  nfilas, ncol,pojf,pojc);
+                gameover (player,numeros,nfilas,ncol);
+                contai=0;
             }
         }
-        if(opcion == 'i')
-    {
 
-        switch(numeros[pojf][pojc])
+
+        if (opcion == 'p')
+        {
+            for(int i=0; i<nfilas; i++)
             {
-            case 1:
-
-                alien(player,numeros,nfilas,ncol,jugf,jugc);
-
-                break;
-            case 2:
-                gameover (player,numeros,nfilas,ncol,jugf,jugc);
-                break;
-            case 3:
-                galleta (player,numeros,nfilas,ncol,jugf,jugc);
-                break;
-            default:
-                player[jugf][jugc]= 'a';
-                numeros[jugf][jugc]= 5;
-                break;
+                for (int j=0; j<ncol; j++)
+                {
+                    cout<<setw(8)<<numeros[i][j];
+                }
+                cout<<endl;
             }
+
         }
-        break;
-    case 'D':
-    case 'd':
 
-
-
-            case 'p':
-
-                    for(int i=0; i<nfilas; i++)
-    {
-        for (int j=0; j<ncol; j++)
-            {
-                cout<<setw(8)<<numeros[i][j];
-            }
-            cout<<endl;
-        }
-        break;
     }
-
-
+    while (opcion !='s');
 }
-void sensacion (  char player [maxf][maxc], int numeros [maxf][maxc], int nfilas, int ncol)
+void sensacion ( char player [maxf][maxc], int numeros [maxf][maxc], int nfilas, int ncol,int pojf,int pojc)
 {
-
-
-
+        if (numeros [pojf][pojc+1]==1)
+        {
+            cout<<"alien"<<endl;
+        }
+}
+void gameover  ( char player [maxf][maxc],int numeros[maxf][maxc], int nfilas, int ncol,int pojf,int pojc)
+{
 
 }
 
